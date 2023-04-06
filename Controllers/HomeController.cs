bdfb649aa6f9b8220d25b4c1bec87902ac95d436
Mgarcia456 <Asvp_miguel36@icloud.com>
2023-04-06 01:33:07 -0400

    Dojo_survey

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Survey.Models;
+
+
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Survey.Controllers
+
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View("index");
+        }
+
+        [HttpPost("result")]
+        public IActionResult Result(string Name, string Location, string Language, string Comment)
+        {
+            ViewBag.Name = Name;
+            ViewBag.Location = Location;
+            ViewBag.Language = Language;
+            ViewBag.Comment = Comment;
+            return View("result");
+        }
+    }
+}
